471b3cb0566bd29ca4f08e679f7ce9a5f13c73be
Jesse Piscaer <jesse.piscaer@adyen.com>
2014-01-14 17:48:36 +0100

    New modifications

--- 4.Modifications/httppost/cancel.php
+++ 4.Modifications/httppost/cancel.php
@@ -22,23 +22,37 @@
    
  $request = array(
     "action" => "Payment.cancel",
-    "modificationRequest.merchantAccount" => "YourMerchantAccount",
-    "modificationRequest.originalReference" => "PspReferenceOfTheAuthorisedPayment",
+    "modificationRequest.merchantAccount" => "JessePiscaerCOM", //YourMerchantAccount
+    "modificationRequest.originalReference" => "8613896220985091", //PspReferenceOfTheAuthorisedPayment
  ); 
   
+  
  $ch = curl_init();
- curl_setopt($ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/adapter/httppost");
- curl_setopt($ch, CURLOPT_HEADER, false);
- curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
- curl_setopt($ch, CURLOPT_USERPWD, "YourWSUser:YourWSUserPassword");
- curl_setopt($ch, CURLOPT_POST,count($request));
- curl_setopt($ch, CURLOPT_POSTFIELDS,$request);
  
+ curl_setopt($ch, CURLOPT_URL, "https://pal-test.adyen.com/pal/adapter/httppost");
+ //curl_setopt($ch, CURLOPT_URL, "http://speeltuin.jessepiscaer.com/notifications-httppost.php"); 
+ curl_setopt($ch, CURLOPT_HEADER, false);
+ curl_setopt($ch, CURLOPT_PORT , 443); 
+ curl_setopt($ch, CURLOPT_VERBOSE, 0); 
+ curl_setopt($ch, CURLOPT_HEADER, 0); 
+ curl_setopt($ch, CURLOPT_SSLVERSION, 3); 
+ curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC  );
+ curl_setopt($ch, CURLOPT_USERPWD, "ws@Company.JessePiscaer:GtrMU{mccv5R34pckSF7<Tdau"); //YourWSUser:YourWSUserPassword
+ curl_setopt($ch, CURLOPT_POST,3);
+ curl_setopt($ch, CURLOPT_POSTFIELDS,"action=Payment.cancel&modificationRequest.merchantAccount=JessePiscaerCOM&modificationRequest.originalReference=8613896220985091");
+ 
+ //curl_setopt($ch, CURLOPT_POST,count($request));
+ //curl_setopt($ch, CURLOPT_POSTFIELDS,$request);
+ 
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_TIMEOUT,4);
+ curl_setopt($ch, CURLOPT_VERBOSE, true);
+  
  $result = curl_exec($ch);
  
  if($result === false)
     echo "Error: " . curl_error($ch);
  else
-    print_r($result);
+    print_r("Result: ".$result);
  
  curl_close($ch);
